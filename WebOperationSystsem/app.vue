a31bf58908b561695d10b6cdad79fbae1d1340a0
KYBEX <artm.22@internet.ru>
2024-04-04 15:22:35 +0300

    first commit

---
+++ WebOperationSystsem/app.vue
@@ -0,0 +1,40 @@
+<template>
+  <div v-if="!loading.loaded" class="loadingloaded">
+    <h1>Loading{{loading.dots}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loading: {
+        loaded: false,
+        dots: '.'
+      }
+    }
+  },
+  methods: {
+    updateDots() {
+      let dots = this.loading.dots;
+      if (dots === '...') {
+        dots = '.';
+      } else if (dots === '..') {
+        dots = '...';
+      } else if (dots === '.') {
+        dots = '..';
+      }
+      this.loading.dots = dots;
+    }
+  },
+  mounted() {
+    let dotsInterval = setInterval(() => {this.updateDots();}, 200);setTimeout(() => {clearInterval(dotsInterval);this.loading.loaded = true;}, 5000);
+
+  }
+}
+</script>
+
+<style>
+.loadingloaded{user-select:none;-webkit-user-select:none;display: flex;flex-direction: column;text-align: center;align-items: center;justify-content: center;min-height: 100vh;position: relative;}
+.loadingloaded h1 {position: absolute;top: 50%;transform: translateY(-50%);font-family: Arial, Helvetica, sans-serif;}
+</style>
