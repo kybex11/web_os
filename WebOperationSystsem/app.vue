c95339113cd613144e82473e4b811f323ecf918b
KYBEX <artm.22@internet.ru>
2024-04-05 21:58:35 +0300

    upbar is component

--- WebOperationSystsem/app.vue
+++ WebOperationSystsem/app.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div v-if="loading.loaded" class="view-container">
       <div class="upbar">
-        <h1>{{time.upbar_clock}}</h1>
+        <upbar/>
       </div>
       <div class="board-container">
         <div class="desktop"></div>
@@ -25,16 +25,10 @@ export default {
       },
       desktop: {
         focusApp: '',
-      },
-      time: {
-        upbar_clock: '00:00:00'
       }
     }
   },
   methods: {
-    updateUpbarClock() {
-      this.time.upbar_clock = new Date().toTimeString().slice(0,8);
-    },
     updateDots() {
       let dots = this.loading.dots;
       if (dots === '...') {
@@ -49,17 +43,12 @@ export default {
   },
   mounted() {
     let dotsInterval = setInterval(() => {this.updateDots();}, 200);setTimeout(() => {clearInterval(dotsInterval);this.loading.loaded = true;}, 5000);
-    this.updateUpbarClock();
-    setInterval(() => {
-      this.updateUpbarClock();
-    }, 1000)
   }
 }
 </script>
 
 <style>
 body {background-color:gray;overflow:hidden;top:0;left:0;padding:0;margin:0;width:100%;height:100%;}
-.upbar {background-color:black;height:40px;width:100%;font-size: 0.5rem;display:flex;justify-content:center;align-items:center;user-select:none;-webkit-user-select:none;}
 .loadingloaded{user-select:none;-webkit-user-select:none;display: flex;flex-direction: column;text-align: center;align-items: center;justify-content: center;min-height: 100vh;position: relative;}
 .loadingloaded h1 {font-size: 4em; text-shadow: 0 0 0.2em rgba(0, 0, 0, 0.8), 0.05em 0.05em 0.1em rgba(0, 0, 0, 0.5);transform:perspective(500px) rotateX(10deg) rotateY(-15deg);transform-style: preserve-3d; color:white;position: relative;top: 50%;transform: translateY(-50%);font-family: Arial, Helvetica, sans-serif;}
 .loadingloaded h1:before {content:"";position:absolute;top: -10px; left: -10px; right: -10px; bottom:-10px;border: 5px solid "#663399";z-index:-1;}
---
+++ WebOperationSystsem/components/upbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>{{time.upbar_clock}}</h1>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      time: {
+        upbar_clock: '00:00:00'
+      }
+    }
+  },
+  methods: {
+    updateUpbarClock() {
+      this.time.upbar_clock = new Date().toTimeString().slice(0,8);
+    },
+  },
+  mounted() {
+    this.updateUpbarClock();
+    setInterval(() => {
+      this.updateUpbarClock();
+    }, 1000)
+  }
+}
+</script>
+
+<style>
+body {background-color:gray;overflow:hidden;top:0;left:0;padding:0;margin:0;width:100%;height:100%;}
+.upbar {background-color:black;height:40px;width:100%;font-size: 0.5rem;display:flex;justify-content:center;align-items:center;user-select:none;-webkit-user-select:none;}
+</style>
