2d187ae450627eb1f0268d07372ac91970cb6d78
KYBEX <artm.22@internet.ru>
2024-04-05 20:26:15 +0300

    Loading page update

--- WebOperationSystsem/app.vue
+++ WebOperationSystsem/app.vue
@@ -10,6 +10,7 @@
     </div>
     <div v-if="!loading.loaded" class="loadingloaded">
       <h1>Loading{{loading.dots}}</h1>
+      <!--<h2>Please don't turn off your PC</h2>-->
     </div>
 </div>
 </template>
@@ -60,6 +61,7 @@ export default {
 body {background-color:gray;overflow:hidden;top:0;left:0;padding:0;margin:0;width:100%;height:100%;}
 .upbar {background-color:black;height:40px;width:100%;font-size: 0.5rem;display:flex;justify-content:center;align-items:center;user-select:none;-webkit-user-select:none;}
 .loadingloaded{user-select:none;-webkit-user-select:none;display: flex;flex-direction: column;text-align: center;align-items: center;justify-content: center;min-height: 100vh;position: relative;}
-.loadingloaded h1 {color:white;position: absolute;top: 50%;transform: translateY(-50%);font-family: Arial, Helvetica, sans-serif;}
+.loadingloaded h1 {font-size: 4em; text-shadow: 0 0 0.2em rgba(0, 0, 0, 0.8), 0.05em 0.05em 0.1em rgba(0, 0, 0, 0.5);transform:perspective(500px) rotateX(10deg) rotateY(-15deg);transform-style: preserve-3d; color:white;position: relative;top: 50%;transform: translateY(-50%);font-family: Arial, Helvetica, sans-serif;}
+.loadingloaded h1:before {content:"";position:absolute;top: -10px; left: -10px; right: -10px; bottom:-10px;border: 5px solid "#663399";z-index:-1;}
 .view-container {color: white;font-family: Arial, Helvetica, sans-serif;margin-top:0;margin-left:0;}
 </style>
